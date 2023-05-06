251230d6f10b1fa58d906019d90dcf9aec58da6c
Diego Braga <diegobraga@hertz.com>
2023-05-06 17:58:01 +0100

    searchbox

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import React from "react";
+import { SearchBox } from "../SearchBox/SearchBox";
 
 const Header: React.FC = () => {
   return (
     <nav>
       <div className="max-w-[1300px] mx-auto px-8 py-4 header">
-        <div className="flex items-center border-2 p-4 rounded-2xl shadow-sm py-6">
+        <div className="flex justify-between items-center border-2 p-4 rounded-2xl shadow-sm py-6">
           <Link
             href="/"
             className="text-gray-600 hover:text-gray-700 text-xl md:text-2xl lg:text-2xl"
           >
             COUNTRIES APP
           </Link>
+          <SearchBox />
         </div>
       </div>
     </nav>
---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SearchBox: React.FC = () => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="px-4 py-2 w-64 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 h-5 w-5 text-gray-600 hover:text-gray-800"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" y1="21" x2="16.65" y2="16.65" />
+      </svg>
+    </div>
+  );
+};
+
+export { SearchBox };
