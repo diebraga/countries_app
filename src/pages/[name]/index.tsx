e9599d5bd5905e1b4b804abc1fe1a1b2e0103220
Diego Braga <diegobraga@hertz.com>
2023-05-07 18:41:47 +0100

    basic seo

--- src/pages/[name]/index.tsx
+++ src/pages/[name]/index.tsx
@@ -4,6 +4,7 @@ import { Country } from "@/@types";
 import { CountryPageItem } from "@/components/CountryPageItem/CountryPageItem";
 import { findCountryByName } from "@/utils/findCountryByName/findCountryByName";
 import { FailedToLoad } from "@/components/FailedToLoad/FailedToLoad";
+import Head from "next/head";
 
 type CountryPageProps = {
   name: string;
@@ -17,7 +18,33 @@ const CountryPage: NextPage<CountryPageProps> = ({ name, data }) => {
     return <FailedToLoad />;
   }
 
-  return <CountryPageItem country={country} />;
+  return (
+    <>
+      <Head>
+        <title>{name} | Countries app</title>
+        <meta
+          name="description"
+          content={`${name} description`}
+        />
+        <meta property="og:title" content={`${name} | Countries app`} />
+        <meta
+          property="og:description"
+          content="Countries app that lists all world countries"
+        />
+        <meta property="og:url" content={``} />
+        <meta property="og:image" content="https://github.com/diebraga.png" />
+        <meta property="og:image:alt" content="countries app image" />
+        <meta name="github:owner" content="diebraga" />
+        <meta
+          name="github:repo"
+          content="https://github.com/diebraga/countries_app"
+        />
+        <meta name="github:image" content="https://github.com/diebraga.png" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CountryPageItem country={country} />
+    </>
+  );
 };
 
 export const getServerSideProps = async ({
